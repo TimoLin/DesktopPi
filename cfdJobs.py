9c3af93ec5a4c8d3866bc4c5f13c447918c7637e
Teng Zhang <ztnuaa0211@gmail.com>
2023-05-21 21:19:02 +0800

    Handle exceptions.

--- cfdJobs.py
+++ cfdJobs.py
@@ -21,49 +21,56 @@ class cfdJobs():
         self.locations = []
 
     def checkUpdate(self):
-        # Make a GET request to the webpage
-        response = requests.get(self.url,headers=headers)
-        
+
         self.titles = []
         self.employers = []
         self.locations = []
-        
-        if response.ok:
-            # Parse the HTML content with BeautifulSoup
-            soup = BeautifulSoup(response.content, 'html.parser')
-            try:
-                # Find all job listings and store them in a list
-                job_table = soup.find('table', {'border': '2', 'cellpadding': '5', 'width': '100%'})
-                job_listings = job_table.find_all('tr')
-          
-                # Iterate over the list of job listings
-                for job_rows in job_listings:
-                    job = job_rows.find_all('td')[0]
-        
-                    # Extract the job title
-                    title = job.find('b').text
-        
-                    # Extract the employer and location of the job 
-                    employer = job.find('i').text
-                    location = job.text.split("\n")[3].lstrip()
-                    #date = job.find('font').text.split(',')[1].strip()
-        
-                    # Print the job listing information
-                    #print(f"Title: {title}\nEmployer: {employer}\nLocation: {location}\n")
-        
-                    # Put resuts to list
-                    self.titles.append(title)
-                    self.employers.append(employer)
-                    self.locations.append(location)
-            except AttributeError as e:
-                self.titles.append("Request Error: Empty")
-                self.employers.append("Request Error: Empty")
-                self.locations.append("Request Error: Empty")
 
-            return True
-        else:
-            # Handle the response error
-            self.titles.append("Url Request Error")
-            self.employers.append("Url Request Error")
-            self.locations.append("Request Error")
-            return False
+        # Make a GET request to the webpage
+        try:
+            response = requests.get(self.url,headers=headers)
+        
+            if response.ok:
+                # Parse the HTML content with BeautifulSoup
+                soup = BeautifulSoup(response.content, 'html.parser')
+                try:
+                    # Find all job listings and store them in a list
+                    job_table = soup.find('table', {'border': '2', 'cellpadding': '5', 'width': '100%'})
+                    job_listings = job_table.find_all('tr')
+              
+                    # Iterate over the list of job listings
+                    for job_rows in job_listings:
+                        job = job_rows.find_all('td')[0]
+            
+                        # Extract the job title
+                        title = job.find('b').text
+            
+                        # Extract the employer and location of the job 
+                        employer = job.find('i').text
+                        location = job.text.split("\n")[3].lstrip()
+                        #date = job.find('font').text.split(',')[1].strip()
+            
+                        # Print the job listing information
+                        #print(f"Title: {title}\nEmployer: {employer}\nLocation: {location}\n")
+            
+                        # Put resuts to list
+                        self.titles.append(title)
+                        self.employers.append(employer)
+                        self.locations.append(location)
+                except AttributeError as e:
+                    self.titles.append("Request Error: Empty")
+                    self.employers.append("Request Error: Empty")
+                    self.locations.append("Request Error: Empty")
+
+                return True
+            else:
+                # Handle the response error
+                self.titles.append("Url Request Error")
+                self.employers.append("Url Request Error")
+                self.locations.append("Request Error")
+                return False
+
+        except requests.exceptions.RequestException as e:
+            self.titles.append("Request Error: Empty")
+            self.employers.append("Request Error: Empty")
+            self.locations.append("Request Error: Empty")
--- githubStatistic.py
+++ githubStatistic.py
@@ -17,23 +17,29 @@ class ghStats():
         '''Get repository number/star/issues/commits
         '''
 
-        repos = self.gh.get_user().get_repos()
-
         totStars  = 0
         totCmts = 0
         totPrs    = 0
         totIssues = 0
+        totRepos = 0
 
-        for repo in repos:
-            try:
-                totStars += repo.stargazers_count
-                totCmts  += repo.get_commits().totalCount
-                prs = repo.get_pulls(state='open')
-                totPrs += prs.totalCount
-                issues = repo.get_issues(state='open')
-                totIssues += issues.totalCount
-            except GithubException:
-                pass
+        try:
+            repos = self.gh.get_user().get_repos()
 
-        totRepos  = repos.totalCount
+            totRepos  = repos.totalCount
+
+            for repo in repos:
+                try:
+                    totStars += repo.stargazers_count
+                    totCmts  += repo.get_commits().totalCount
+                    prs = repo.get_pulls(state='open')
+                    totPrs += prs.totalCount
+                    issues = repo.get_issues(state='open')
+                    totIssues += issues.totalCount
+                except GithubException as err:
+                    pass
+
+        except GithubException as error:
+            pass
+
         return [totStars, totCmts, totPrs, totIssues, totRepos]
